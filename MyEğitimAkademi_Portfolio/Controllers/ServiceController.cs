2241668b52bdb730997de5cc68264235ddb6af02
aydinmfatih <aydinmfatih@gmail.com>
2023-08-03 21:55:41 +0300

    updt

---
+++ MyEğitimAkademi_Portfolio/Controllers/ServiceController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyEğitimAkademi_Portfolio.Models;
+
+namespace MyEğitimAkademi_Portfolio.Controllers
+{
+    public class ServiceController : Controller
+    {
+        MyPortfolioDbEntities db = new MyPortfolioDbEntities();
+
+        public ActionResult Index()
+        {
+            var values = db.Service.ToList();
+            return View(values);
+        }
+        public ActionResult AddService()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ MyEğitimAkademi_Portfolio/Controllers/TestimonialController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyEğitimAkademi_Portfolio.Controllers
+{
+    public class TestimonialController : Controller
+    {
+        // GET: Testimonial
+        public ActionResult Index()
+        {
+            return View();
+        } 
+        public ActionResult AddTestimonial()
+        {
+            return View();
+        }
+    }
+}
