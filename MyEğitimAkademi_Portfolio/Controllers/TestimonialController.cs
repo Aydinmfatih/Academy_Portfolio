62fbd9dedb2776c9b2eac03f5fc643fe49be3319
aydinmfatih <aydinmfatih@gmail.com>
2023-08-09 22:51:15 +0300

    updt

--- MyEğitimAkademi_Portfolio/Controllers/TestimonialController.cs
+++ MyEğitimAkademi_Portfolio/Controllers/TestimonialController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyEğitimAkademi_Portfolio.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,14 +9,50 @@ namespace MyEğitimAkademi_Portfolio.Controllers
 {
     public class TestimonialController : Controller
     {
-        // GET: Testimonial
+        MyPortfolioDbEntities db = new MyPortfolioDbEntities();
         public ActionResult Index()
         {
-            return View();
-        } 
+            var values = db.Testimonial.ToList();
+            return View(values);
+        }
+        [HttpGet]
         public ActionResult AddTestimonial()
         {
             return View();
         }
+        [HttpPost]
+        public ActionResult AddTestimonial(Testimonial testimonial)
+        {
+            db.Testimonial.Add(testimonial);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DeleteTestimonial(int id)
+        {
+            var value = db.Testimonial.Find(id);
+            db.Testimonial.Remove(value);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult UpdateTestimonial(int id)
+        {
+            var value = db.Testimonial.Find(id);
+            return View(value);
+        }
+        [HttpPost]
+        public ActionResult UpdateTestimonial(Testimonial testimonial)
+        {
+            var value = db.Testimonial.Find(testimonial.TestimonialID);
+            value.NameSurname = testimonial.NameSurname;
+            value.Title = testimonial.Title;
+            value.ImageUrl = testimonial.ImageUrl;
+            value.Comment = testimonial.Comment;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
+
 }
